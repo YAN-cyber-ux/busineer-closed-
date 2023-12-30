54fc59c86478df6b47ca0642c40da7979f2fab2b
yanger <kanayaofficial1129@gmail.com>
2023-12-30 20:23:55 +0900

    add top page

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,12 @@ module Busineer
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.generators do |g|
+      g.skip_routes true
+      g.assets false
+      g.helper false
+      g.test_framework false
+    end
   end
 end
